1ff50164a3b00018c6e23f486111e845d7564d43
tda25 <tda25@cornell.edu>
2020-10-15 15:50:41 -0400

    Updates

---
+++ precache-manifest.fb4f507bae0394a50675c1a3c3062d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddb9f5eef56b932ceb46eeb874fdcc2",
+    "url": "/tda25/reenvision-app/index.html"
+  },
+  {
+    "revision": "151bacf597e78a5b56d8",
+    "url": "/tda25/reenvision-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "479eb3c8c4d7f91f7acf",
+    "url": "/tda25/reenvision-app/static/js/2.a4e37168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tda25/reenvision-app/static/js/2.a4e37168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151bacf597e78a5b56d8",
+    "url": "/tda25/reenvision-app/static/js/main.ce062ddd.chunk.js"
+  },
+  {
+    "revision": "53ade2af345aa2878dc6",
+    "url": "/tda25/reenvision-app/static/js/runtime-main.65c643a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tda25/reenvision-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
